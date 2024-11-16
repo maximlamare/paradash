28045885a856b2d2cc22d73a89a9e5282d34146a
Maxim Lamare <maxim.lamare@pm.me>
2024-11-16 13:24:05 +0100

    Add functionalities to the Gear page

--- src/components/gearOverview.vue
+++ src/components/gearOverview.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="bg-gray-100 dark:bg-gray-900 min-h-screen p-8">
+  <div class="bg-gray-100 dark:bg-gray-900 min-h-screen p-8 w-full">
     <div class="flex justify-between items-center mb-8">
       <button
         @click="openAddModal"
-        class="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600"
+        class="button-blue"
       >
         Add Gear
       </button>
       <h1 class="text-3xl font-bold text-gray-900 dark:text-gray-100 mx-auto">Gear Overview</h1>
     </div>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div class="w-full flex flex-wrap -mx-2">
       <div
         v-for="category in groupedEquipment"
         :key="category.name"
-        class="col-span-1"
+        class="w-full sm:w-1/2 lg:w-1/3 px-2 mb-4"
       >
         <h2 class="text-2xl font-semibold mb-4 text-gray-900 dark:text-gray-100">{{ category.name }}</h2>
         <div
@@ -23,7 +23,9 @@
         >
           <div class="p-4">
             <div class="flex justify-between items-center">
-              <h3 class="text-xl font-semibold mb-2 text-gray-900 dark:text-gray-100">{{ item.brand }} {{ item.model }}</h3>
+              <h3 class="text-xl font-semibold mb-2 text-gray-900 dark:text-gray-100">
+                {{ item.brand }} {{ item.model }}<span v-if="item.manufacturing_date"> ({{ new Date(item.manufacturing_date).getFullYear() }})</span>
+              </h3>
               <button
                 @click="editEntry(item)"
                 class="hover:bg-blue-600 rounded-lg"
@@ -36,18 +38,16 @@
               </button>
             </div>
             <p class="text-gray-500 dark:text-gray-400 text-sm mb-4">
-              Purchase Date: <span class="font-bold text-indigo-600 dark:text-indigo-400">{{ item.purchase_date }}</span>
+              Purchase Date: <span
+                v-if="item.purchase_date"
+                class="font-bold text-indigo-600 dark:text-indigo-400"
+              >{{ item.purchase_date }}</span>
+              <span v-else>?</span>
             </p>
-            <p
-              v-if="item.total_flight_time"
-              class="text-gray-700 dark:text-gray-300"
-            >
+            <p class="text-gray-700 dark:text-gray-300">
               <strong>Total Flight Time:</strong> {{ item.total_flight_time }} hours
             </p>
-            <p
-              v-if="item.number_of_flights"
-              class="text-gray-700 dark:text-gray-300"
-            >
+            <p class="text-gray-700 dark:text-gray-300">
               <strong>Number of Flights:</strong> {{ item.number_of_flights }}
             </p>
           </div>
@@ -60,16 +60,32 @@
       @close="showAddModal = false"
       title="Add Gear"
     >
-      <form @submit.prevent="addGlider">
+      <form @submit.prevent="addGear">
+        <div class="mb-4">
+          <label
+            for="gear_type"
+            class="modal-text"
+          >Gear type</label>
+          <select
+            v-model="newGear.gear_type"
+            id="gear_type"
+            class="modal-dropdown"
+          >
+            <option value="glider">Glider</option>
+            <option value="harness">Harness</option>
+            <option value="rescue">Rescue</option>
+          </select>
+        </div>
+
         <div class="mb-4">
           <label
             for="brand"
             class="modal-text"
           >Brand</label>
           <select
-            v-model="newGlider.brand"
+            v-model="newGear.brand"
             id="brand"
-            class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
+            class="modal-dropdown"
             @change="checkNewBrand"
           >
             <option
@@ -86,8 +102,8 @@
             <input
               ref="newBrandInput"
               v-model="newBrandName"
-              placeholder="Enter new glider brand"
-              class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300 mt-2"
+              placeholder="Enter new gear brand"
+              class="modal-dropdown mt-2"
             >
             <button
               @click="addNewBrand"
@@ -102,9 +118,9 @@
             class="modal-text"
           >Model</label>
           <select
-            v-model="newGlider.model"
+            v-model="newGear.model"
             id="model"
-            class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
+            class="modal-dropdown"
             @change="checkNewModel"
           >
             <option
@@ -121,8 +137,8 @@
             <input
               ref="newModelInput"
               v-model="newModelName"
-              placeholder="Enter new glider model"
-              class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300 mt-2"
+              placeholder="Enter new gear model"
+              class="modal-dropdown mt-2"
             >
             <button
               @click="addNewModel"
@@ -133,56 +149,32 @@
         </div>
         <div class="mb-4">
           <label
+            for="manufacturing_date"
+            class="modal-text"
+          >Manufacturing Date</label>
+          <input
+            v-model="newGear.manufacturing_date"
+            id="manufacturing_date"
+            type="date"
+            class="modal-dropdown"
+          >
+        </div>
+        <div class="mb-4">
+          <label
             for="purchase_date"
             class="modal-text"
           >Purchase Date</label>
           <input
-            v-model="newGlider.purchase_date"
+            v-model="newGear.purchase_date"
             id="purchase_date"
             type="date"
-            class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
+            class="modal-dropdown"
           >
         </div>
-        <div class="mb-4">
-          <label
-            for="logo_path"
-            class="modal-text"
-          >Logo Path</label>
-          <input
-            v-model="newGlider.logo_path"
-            id="logo_path"
-            type="text"
-            class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
-          >
-        </div>
-        <div class="mb-4">
-          <label
-            for="total_flight_time"
-            class="modal-text"
-          >Total Flight Time</label>
-          <input
-            v-model="newGlider.total_flight_time"
-            id="total_flight_time"
-            type="number"
-            class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
-          >
-        </div>
-        <div class="mb-4">
-          <label
-            for="number_of_flights"
-            class="modal-text"
-          >Number of Flights</label>
-          <input
-            v-model="newGlider.number_of_flights"
-            id="number_of_flights"
-            type="number"
-            class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
-          >
-        </div>
-        <div class="flex justify-end">
+        <div class="flex justify-center">
           <button
             type="submit"
-            class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+            class="button-blue"
           >Add</button>
         </div>
       </form>
@@ -193,16 +185,31 @@
       @close="showEditModal = false"
       title="Edit Gear"
     >
-      <form @submit.prevent="updateGlider">
+      <form @submit.prevent="updateGear">
+        <div class="mb-4">
+          <label
+            for="edit_type"
+            class="modal-text"
+          >Type</label>
+          <select
+            v-model="editGear.gear_type"
+            id="edit_type"
+            class="modal-dropdown"
+          >
+            <option value="glider">Glider</option>
+            <option value="harness">Harness</option>
+            <option value="rescue">Rescue</option>
+          </select>
+        </div>
         <div class="mb-4">
           <label
             for="edit_brand"
             class="modal-text"
           >Brand</label>
           <select
-            v-model="editGlider.brand"
+            v-model="editGear.brand"
             id="edit_brand"
-            class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
+            class="modal-dropdown"
             @change="checkNewBrand"
           >
             <option
@@ -219,8 +226,8 @@
             <input
               ref="newBrandInput"
               v-model="newBrandName"
-              placeholder="Enter new glider brand"
-              class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300 mt-2"
+              placeholder="Enter new gear brand"
+              class="modal-dropdown mt-2"
             >
             <button
               @click="addNewBrand"
@@ -235,9 +242,9 @@
             class="modal-text"
           >Model</label>
           <select
-            v-model="editGlider.model"
+            v-model="editGear.model"
             id="edit_model"
-            class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
+            class="modal-dropdown"
             @change="checkNewModel"
           >
             <option
@@ -254,8 +261,8 @@
             <input
               ref="newModelInput"
               v-model="newModelName"
-              placeholder="Enter new glider model"
-              class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300 mt-2"
+              placeholder="Enter new gear model"
+              class="modal-dropdown mt-2"
             >
             <button
               @click="addNewModel"
@@ -266,61 +273,37 @@
         </div>
         <div class="mb-4">
           <label
+            for="edit_manufacturing_date"
+            class="modal-text"
+          >Manufacturing Date</label>
+          <input
+            v-model="editGear.manufacturing_date"
+            id="edit_manufacturing_date"
+            type="date"
+            class="modal-dropdown"
+          >
+        </div>
+        <div class="mb-4">
+          <label
             for="edit_purchase_date"
             class="modal-text"
           >Purchase Date</label>
           <input
-            v-model="editGlider.purchase_date"
+            v-model="editGear.purchase_date"
             id="edit_purchase_date"
             type="date"
-            class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
-          >
-        </div>
-        <div class="mb-4">
-          <label
-            for="edit_logo_path"
-            class="modal-text"
-          >Logo Path</label>
-          <input
-            v-model="editGlider.logo_path"
-            id="edit_logo_path"
-            type="text"
-            class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
-          >
-        </div>
-        <div class="mb-4">
-          <label
-            for="edit_total_flight_time"
-            class="modal-text"
-          >Total Flight Time</label>
-          <input
-            v-model="editGlider.total_flight_time"
-            id="edit_total_flight_time"
-            type="number"
-            class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
-          >
-        </div>
-        <div class="mb-4">
-          <label
-            for="edit_number_of_flights"
-            class="modal-text"
-          >Number of Flights</label>
-          <input
-            v-model="editGlider.number_of_flights"
-            id="edit_number_of_flights"
-            type="number"
-            class="w-full px-3 py-2 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
+            class="modal-dropdown"
           >
         </div>
         <div class="flex justify-center space-x-4">
           <button
             type="button"
-            @click="deleteGlider(editGlider.id)"
-            class="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600"
+            @click="deleteGear(editGear.id)"
+            class="button-delete"
           >Delete</button>
           <button
             type="submit"
-            class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+            class="button-blue"
           >Save</button>
         </div>
       </form>
@@ -348,20 +331,22 @@ export default {
       newBrandName: "",
       newModel: false,
       newModelName: "",
-      newGlider: {
+      newGear: {
+        gear_type: "glider", // Default type
         brand: "",
         model: "",
+        manufacturing_date: "",
         purchase_date: "",
-        logo_path: "",
         total_flight_time: 0,
         number_of_flights: 0,
       },
-      editGlider: {
+      editGear: {
         id: null,
+        gear_type: "", // Default type
         brand: "",
         model: "",
+        manufacturing_date: "",
         purchase_date: "",
-        logo_path: "",
         total_flight_time: 0,
         number_of_flights: 0,
       },
@@ -373,16 +358,16 @@ export default {
   methods: {
     fetchEquipment() {
       axios
-        .get("http://localhost:3000/gliders")
+        .get("http://localhost:3000/gear")
         .then((response) => {
-          const gliders = response.data.data.map((item) => ({
+          const gear = response.data.data.map((item) => ({
             ...item,
-            category: "Gliders",
+            category: "Gear",
           }));
-          this.equipment = gliders; // Reset the equipment array before appending new data
+          this.equipment = gear; // Reset the equipment array before appending new data
         })
         .catch((error) => {
-          console.error("Error fetching gliders:", error);
+          console.error("Error fetching gear:", error);
         });
 
       axios
@@ -413,7 +398,7 @@ export default {
     },
     fetchBrandsAndModels() {
       axios
-        .get("http://localhost:3000/gliders")
+        .get("http://localhost:3000/gear")
         .then((response) => {
           const brands = [
             ...new Set(response.data.data.map((item) => item.brand)),
@@ -435,7 +420,11 @@ export default {
     checkNewBrand(event) {
       if (event.target.value === "new") {
         this.newBrand = true;
-        this.newGlider.brand = "";
+        if (this.showAddModal) {
+          this.newGear.brand = "";
+        } else if (this.showEditModal) {
+          this.editGear.brand = "";
+        }
         nextTick(() => {
           this.$refs.newBrandInput.focus();
         });
@@ -446,7 +435,11 @@ export default {
     addNewBrand() {
       if (this.newBrandName.trim() !== "") {
         this.brands.push(this.newBrandName);
-        this.newGlider.brand = this.newBrandName;
+        if (this.showAddModal) {
+          this.newGear.brand = this.newBrandName;
+        } else if (this.showEditModal) {
+          this.editGear.brand = this.newBrandName;
+        }
         this.newBrand = false;
         this.newBrandName = "";
       }
@@ -454,7 +447,11 @@ export default {
     checkNewModel(event) {
       if (event.target.value === "new") {
         this.newModel = true;
-        this.newGlider.model = "";
+        if (this.showAddModal) {
+          this.newGear.model = "";
+        } else if (this.showEditModal) {
+          this.editGear.model = "";
+        }
         nextTick(() => {
           this.$refs.newModelInput.focus();
         });
@@ -465,72 +462,81 @@ export default {
     addNewModel() {
       if (this.newModelName.trim() !== "") {
         this.models.push(this.newModelName);
-        this.newGlider.model = this.newModelName;
+        if (this.showAddModal) {
+          this.newGear.model = this.newModelName;
+        } else if (this.showEditModal) {
+          this.editGear.model = this.newModelName;
+        }
         this.newModel = false;
         this.newModelName = "";
       }
     },
-    addGlider() {
+
+    addGear() {
       axios
-        .post("http://localhost:3000/gliders", this.newGlider)
+        .post("http://localhost:3000/gear", this.newGear)
         .then(() => {
           this.fetchEquipment(); // Refetch equipment to update the UI
           this.showAddModal = false;
-          this.newGlider = {
+          this.newGear = {
+            gear_type: "glider",
             brand: "",
             model: "",
+            manufacturing_date: "",
             purchase_date: "",
-            logo_path: "",
             total_flight_time: 0,
             number_of_flights: 0,
           };
         })
         .catch((error) => {
-          console.error("Error adding glider:", error);
+          console.error("Error adding gear:", error);
         });
     },
     editEntry(item) {
-      this.editGlider = { ...item };
+      this.editGear = { ...item };
+      this.fetchBrandsAndModels(); // Fetch brands and models to ensure dropdowns are populated
       this.showEditModal = true;
     },
-    updateGlider() {
+    updateGear() {
       axios
-        .put(
-          `http://localhost:3000/gliders/${this.editGlider.id}`,
-          this.editGlider
-        )
+        .put(`http://localhost:3000/gear/${this.editGear.id}`, this.editGear)
         .then(() => {
           this.fetchEquipment(); // Refetch equipment to update the UI
           this.showEditModal = false;
         })
         .catch((error) => {
-          console.error("Error updating glider:", error);
+          console.error("Error updating gear:", error);
         });
     },
-    deleteGlider(id) {
+    deleteGear(id) {
       axios
-        .delete(`http://localhost:3000/gliders/${id}`)
+        .delete(`http://localhost:3000/gear/${id}`)
         .then(() => {
           // Remove the deleted item from the equipment array
           this.equipment = this.equipment.filter((item) => item.id !== id);
           this.showEditModal = false;
         })
         .catch((error) => {
-          console.error("Error deleting glider:", error);
+          console.error("Error deleting gear:", error);
         });
     },
   },
   computed: {
     groupedEquipment() {
+      const typeNames = {
+        glider: "Gliders",
+        harness: "Harnesses",
+        rescue: "Rescues",
+      };
       const categories = {};
       this.equipment.forEach((item) => {
-        if (!categories[item.category]) {
-          categories[item.category] = {
-            name: item.category,
+        if (!categories[item.gear_type]) {
+          categories[item.gear_type] = {
+            name: typeNames[item.gear_type],
             items: [],
           };
         }
-        categories[item.category].items.push(item);
+        categories[item.gear_type].items.push(item);
       });
       return Object.values(categories);
     },
@@ -540,4 +546,5 @@ export default {
 
 <style scoped>
 @import "@/assets/modal.css";
+@import "@/assets/buttons.css";
 </style>
