a28a69490b26ce2dfc6efbafa0be5916581242f7
Maxim Lamare <maxim.lamare@pm.me>
2024-11-30 13:19:12 +0100

    Update settings

--- backend/apiServer.js
+++ backend/apiServer.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const port = 3002;
@@ -17,6 +19,41 @@ app.use(
     logLevel: "debug",
   })
 );
+
+// For settings file
+const settingsFilePath = path.join(__dirname, "appSettings.json");
+
+app.post("/save-settings", (req, res) => {
+  const { categories, types, gliderWarningDuration, rescueWarningDuration } =
+    req.body;
+  fs.writeFile(
+    settingsFilePath,
+    JSON.stringify({
+      categories,
+      types,
+      gliderWarningDuration,
+      rescueWarningDuration,
+    }),
+    (err) => {
+      if (err) {
+        res.status(500).json({ error: "Failed to save categories" });
+        return;
+      }
+      res.json({ message: "Categories saved successfully" });
+    }
+  );
+});
+
+app.get("/get-settings", (req, res) => {
+  fs.readFile(settingsFilePath, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).json({ error: "Failed to read categories" });
+      return;
+    }
+    const settings = JSON.parse(data);
+    res.json(settings);
+  });
+});
 
 app.listen(port, () => {
   console.log(`API proxy server running on port ${port}`);
--- src/components/settings.vue
+++ src/components/settings.vue
@@ -1,45 +1,143 @@
 <template>
   <div class="page-bg-grey">
-    <h1 class="text-3xl font-bold text-gray-900 dark:text-gray-100 mx-auto">
-      Settings
-    </h1>
-    <div class="mb-4">
-      <label for="categories" class="modal-text">Categories</label>
-      <textarea
-        id="categories"
-        v-model="categoriesInput"
-        class="modal-dropdown w-full"
-        placeholder="Enter categories separated by commas"
-      ></textarea>
+    <div class="flex justify-between items-center mb-8">
+      <h1 class="page-title1">Gear Overview</h1>
     </div>
-    <button @click="saveCategories" class="button-blue">Save</button>
+    <div class="flex justify-end mb-8">
+      <button @click="saveSettings" class="button-blue">Save settings</button>
+    </div>
+    <div class="mt-12 mb-12">
+      <label for="categories" class="page-title2">Categories</label>
+
+      <div
+        v-for="(category, index) in categories"
+        :key="index"
+        class="flex items-center mt-4 mb-2"
+      >
+        <input
+          type="text"
+          v-model="categories[index]"
+          class="modal-dropdown w-1/4"
+        />
+        <button @click="removeCategory(index)" class="button-delete ml-2">
+          Remove
+        </button>
+      </div>
+      <div>
+        <button @click="addCategory" class="button-blue mt-2">
+          Add Category
+        </button>
+      </div>
+    </div>
+    <div class="mt-12 mb-12">
+      <label for="types" class="page-title2">Types</label>
+      <div
+        v-for="(type, index) in types"
+        :key="index"
+        class="flex items-center mt-4 mb-2"
+      >
+        <input
+          type="text"
+          v-model="types[index]"
+          class="modal-dropdown w-1/4"
+        />
+        <button @click="removeType(index)" class="button-delete ml-2">
+          Remove
+        </button>
+      </div>
+      <div>
+        <button @click="addType" class="button-blue mt-2">Add Type</button>
+      </div>
+    </div>
+    <div class="mt-12 mb-12">
+      <label for="warningDuration" class="page-title2"
+        >Glider Warning Duration (months)</label
+      >
+      <div>Set the reminder time for your glider check</div>
+      <div>
+        <input
+          type="number"
+          v-model="gliderWarningDuration"
+          class="modal-dropdown w-1/4 mt-2"
+        />
+      </div>
+    </div>
+    <div class="mt-12 mb-12">
+      <label for="warningDuration" class="page-title2"
+        >Rescue Warning Duration (months)</label
+      >
+      <div>Set the reminder time for your rescue check</div>
+      <div>
+        <input
+          type="number"
+          v-model="rescueWarningDuration"
+          class="modal-dropdown w-1/4 mt-2"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
+import axios from "axios";
 
 export default {
   name: "SettingsPage",
   data() {
     return {
-      categoriesInput: "",
+      categories: [],
+      types: [],
+      gliderWarningDuration: 12,
+      rescueWarningDuration: 12,
     };
   },
-  computed: {
-    ...mapGetters(["getCategories"]),
-  },
+  computed: {},
   methods: {
-    ...mapActions(["updateCategories"]),
-    saveCategories() {
-      const categories = this.categoriesInput
-        .split(",")
-        .map((cat) => cat.trim());
-      this.updateCategories(categories);
+    saveSettings() {
+      axios
+        .post("http://localhost:3002/save-settings", {
+          categories: this.categories,
+          types: this.types,
+          gliderWarningDuration: this.gliderWarningDuration,
+          rescueWarningDuration: this.rescueWarningDuration,
+        })
+        .then(() => {
+          this.fetchSettings();
+        })
+        .catch((error) => {
+          console.error("Error saving categories:", error);
+        });
+    },
+    addCategory() {
+      this.categories.push("");
+      this.updateSettings(this.categories, this.types);
+    },
+    removeCategory(index) {
+      this.categories.splice(index, 1);
+    },
+    addType() {
+      this.types.push("");
+      this.updateSettings(this.categories, this.types);
+    },
+    removeType(index) {
+      this.types.splice(index, 1);
+    },
+    fetchSettings() {
+      axios
+        .get("http://localhost:3002/get-settings")
+        .then((response) => {
+          this.categories = response.data.categories;
+          this.types = response.data.types;
+          this.gliderWarningDuration = response.data.gliderWarningDuration;
+          this.rescueWarningDuration = response.data.rescueWarningDuration;
+        })
+        .catch((error) => {
+          console.error("Error fetching settings:", error);
+        });
     },
   },
   created() {
-    this.categoriesInput = this.getCategories.join(", ");
+    this.fetchSettings();
   },
 };
 </script>
